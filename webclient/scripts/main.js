d6905cbdb74576f9cb28ba2e004101454a736c84
Robert Porter <rob@gradecam.com>
2013-11-08 14:42:44 -0800

    Added config button

--- webclient/scripts/main.js
+++ webclient/scripts/main.js
@@ -1,3 +1,6 @@
 define(['fftDisplay', 'controlBar'], function(fftDisplay, controlBar) {
+    $("#configButton").click(function() {
+        window.open("/config", "_blank", "width=400,height=500");
+    });
 });
 
